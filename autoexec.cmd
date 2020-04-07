c563ac71584e34b94c081f339ab734561a9c1193
frank-e <hmdmhdfmhdjmzdtjmzdtzktdkztdjz@gmail.com>
2020-04-07 15:10:09 +0200

    Update autoexec.cmd

--- autoexec.cmd
+++ autoexec.cmd
@@ -5,12 +5,12 @@ doskey 8888=ping -t 8.8.8.8
 doskey cool=taskkill /f /im chrome.exe$Tresmon.exe
 doskey wifi=netsh int set int WiFi DISABLED$tnetsh int set int WiFi ENABLED
 doskey tops=resmon.exe $*
-doskey posh=PowerShell.exe $*
 doskey tatort=youtube-dl -f http-2 $*
 doskey ffmpeg=ansicon -m ffmpeg.exe -hide_banner $*
 doskey ffprobe=ansicon -m ffprobe.exe -hide_banner $*
-doskey paste=for /F "tokens=*" %%T in ('powershell Get-Clipboard') do @echo %%T
+doskey paste=for /F "tokens=*" %%T in ('pwsh Get-Clipboard') do @echo %%T
 doskey wmk=for %%D in (%%CD%%) do wmake -h -f %%~nD.wmk $*
+doskey ydl=youtube-dl.exe $*
 doskey 7za=@"%ProgramW6432%\7-ZIP\7za.exe" $*
 doskey git=@"%ProgramW6432%\git\cmd\git.exe" $*
 doskey diff=@"%ProgramW6432%\git\usr\bin\diff.exe" $*
@@ -23,7 +23,9 @@ doskey checksur=@Dism.exe /Online /Cleanup-Image /ScanHealth $*
 doskey xedit=@"%ProgramFiles(x86)%\KEDITW\KEDITW32.exe" $*
 doskey xnview=@"%ProgramFiles(x86)%\XnView\XnView.exe" $*
 doskey nconvert=@"%ProgramFiles(x86)%\XnView\nconvert.exe" $*
+doskey oorexx=@"%REXX_HOME%\rexx.exe" $*
+doskey rexxec=@"%REXX_HOME%\rexx.exe" -e $*
+doskey torexx=echo $*$b@"%REGINA_LANG_DIR%\regina.exe"
 doskey regina=@"%REGINA_LANG_DIR%\regina.exe" $*
-doskey noutil=@"%REGINA_LANG_DIR%\rexx.exe" $*
-doskey regimm=echo $*$b@"%REGINA_LANG_DIR%\regina.exe"
+doskey rexx=@"%REGINA_LANG_DIR%\rexx.exe" $*
 doskey /insert
